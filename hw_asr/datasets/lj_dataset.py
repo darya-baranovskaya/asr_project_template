c4d5e84015d8dd1897d427307e472704cb3fc111
Дарья Барановская <darya.baranovskaya01082001@gmail.com>
2021-10-21 00:37:13 +0200

    lj dataset

--- hw_asr/datasets/lj_dataset.py
+++ hw_asr/datasets/lj_dataset.py
@@ -4,7 +4,7 @@ from hw_asr.base.base_dataset import BaseDataset
 
 
 class LJDataset(BaseDataset):
-    def __init__(self, audio_dir, transcription_dir=None, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         data = load_dataset("lj_speech")
         super().__init__(data, *args, **kwargs)
 
