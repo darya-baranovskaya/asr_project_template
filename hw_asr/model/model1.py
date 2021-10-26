3ea51150ad2665c9bdace969a75113ebcf0a1fee
Дарья Барановская <darya.baranovskaya01082001@gmail.com>
2021-10-26 04:03:43 +0300

    model

--- hw_asr/model/model1.py
+++ hw_asr/model/model1.py
@@ -1,5 +1,6 @@
 from torch import nn
 from torch.nn import Sequential
+from torch import rand
 
 from hw_asr.base import BaseModel
 
@@ -9,14 +10,17 @@ class MainModel1(BaseModel):
         self.bn1 = nn.BatchNorm1d(n_feats)
         self.attn = nn.MultiheadAttention(n_feats, num_heads, dropout=0.0, bias=True, batch_first=True)
         self.gru = nn.GRU(n_feats, fc_hidden, num_layers=num_layers,  bidirectional=True)
-        self.fc = nn.Linear(fc_hidden * 2, n_class)
+        self.fc = nn.Sequential(
+                  nn.Linear(in_features=fc_hidden * 2, out_features=fc_hidden),
+                  nn.LeakyReLU(),
+                  nn.Linear(in_features=fc_hidden, out_features=fc_hidden))
 
     def forward(self, spectrogram, *args, **kwargs):
-        x = self.bn1(spectrogram.permute(0, 2, 1)).permute(0, 2, 1)
+        x = spectrogram.permute(0, 2, 1)
+        # x = self.bn1(spectrogram.permute(0, 2, 1)).permute(0, 2, 1)
         # print(x.shape)
-        # x = spectrogram
-        x = self.attn(x, x, x)
-        x, _ = self.gru(spectrogram)
+        # x, _ = self.attn(x, x, x)
+        x, _ = self.gru(x)
         x = self.fc(x)
         # print('output size=', x.size())
         return  {"logits": x}
